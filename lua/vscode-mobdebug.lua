61780e3c97dffc882fad0c01a2fc680545b68d75
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2022-02-23 21:34:41 +0300

    Add mobdebug.lua file

--- lua/vscode-mobdebug.lua
+++ lua/vscode-mobdebug.lua
@@ -1,13 +1,1 @@
--- TODO: implement real adapter for the MobDebug module
--- For now uses module form the [lua-debug](https://github.com/devcat-studio/VSCodeLuaDebug) module
-
-local json = require 'dkjson'
-local debuggee = require 'vscode-debuggee'
-
-local vscodeMobDebug = {}
-
-function vscodeMobDebug.start(host, port)
-  return debuggee.start(json, {controllerHost = host, controllerPort = port})
-end
-
-return vscodeMobDebug
+return require 'mobdebug'
